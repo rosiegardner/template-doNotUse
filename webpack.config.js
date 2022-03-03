09480dd9cb254f8ec58cca0d1cba7375e7b2bdd8
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-02 20:10:02 -0800

    add html-webpack-plugin@3.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
+
 };
